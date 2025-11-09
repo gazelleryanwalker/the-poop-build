a691116d9100445cb2cb3cc579b66fe82b282d5d
gazelleryanwalker <59974365+gazelleryanwalker@users.noreply.github.com>
2025-11-09 17:11:48 -0500

    Add Decap CMS for content management

--- sanity/schemas/index.ts
+++
@@ -1,9 +0,0 @@
-import { homepage } from "./homepage"
-import { aboutPage } from "./aboutPage"
-import { practiceArea } from "./practiceArea"
-import { caseResult } from "./caseResult"
-import { blogPost } from "./blogPost"
-import { attorney } from "./attorney"
-import { siteSettings } from "./siteSettings"
-
-export const schemaTypes = [homepage, aboutPage, practiceArea, caseResult, blogPost, attorney, siteSettings]
--- tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ cacheDir: '/home/ubuntu/the-poop-build/tina/__generated__/.cache/1762708220203', url: 'http://localhost:4001/graphql', token: 'undefined', queries,  });
-export default client;
-  
